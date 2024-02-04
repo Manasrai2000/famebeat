296756cb26620c0566400692fd8c9b54b12d5c57
Manasrai2000 <manasrai123400@gmail.com>
2024-02-04 19:31:08 +0530

    make correction and remove useless code

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,18 @@
 // 
-document.addEventListener("DOMContentLoaded", function(){
-            
+document.addEventListener("DOMContentLoaded", function () {
+
     el_autohide = document.querySelector('.autohide');
-    
+
     // add padding-top to bady (if necessary)
     navbar_height = document.querySelector('.navbar').offsetHeight;
     document.body.style.paddingTop = navbar_height + 'px';
 
-    if(el_autohide){
-        
+    if (el_autohide) {
+
         var last_scroll_top = 0;
-        window.addEventListener('scroll', function() {
-               let scroll_top = window.scrollY;
-           if(scroll_top < last_scroll_top) {
+        window.addEventListener('scroll', function () {
+            let scroll_top = window.scrollY;
+            if (scroll_top < last_scroll_top) {
                 el_autohide.classList.remove('scrolled-down');
                 el_autohide.classList.add('scrolled-up');
             }
@@ -22,68 +22,23 @@ document.addEventListener("DOMContentLoaded", function(){
             }
             last_scroll_top = scroll_top;
 
-        }); 
+        });
         // window.addEventListener
 
     }
     // if
-    
-}); 
-// DOMContentLoaded  end
 
-
-var swiper = new Swiper('.swipermain', {
-    slidesPerView: 1,
-    centeredSlides: true,
-    loop: true,
-    spaceBetween: 20,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + (index + 1) + '</span>';
-        },
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-        delay: 10000,
-    },
 });
 
 
-var swiper = new Swiper('.swiperteam', {
-    slidesPerView: 'auto', // Set initial slides per view
-    spaceBetween: 10,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      768: {
-        slidesPerView: 2, 
-      },
-      992: {
-        slidesPerView: 3, 
-      },
-      1200: {
-        slidesPerView: 4,
-      },
-    },
-  });
 
-  // footer
-  var swiper = new Swiper('.swiperfooter', {
+
+//  for hero section 
+var swiper = new Swiper('.swiperherosec', {
     slidesPerView: 1,
     centeredSlides: true,
     loop: true,
-    spaceBetween: 20,
+    spaceBetween: 0,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -96,6 +51,40 @@ var swiper = new Swiper('.swiperteam', {
         prevEl: '.swiper-button-prev',
     },
     autoplay: {
-        delay: 5000,
+        delay: 3000,
+    },
+    speed: 1000,
+});
+// for article section 
+var swiper = new Swiper('.swiperarticle', {
+    slidesPerView: 3,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 0,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 3000,
+    },
+    speed: 1000,
+    breakpoints: {
+        980: {
+            slidesPerView: 3,
+        },
+        720: {
+            slidesPerView: 2,
+        },
+        280: {
+            slidesPerView: 1,
+        }
     },
 });
